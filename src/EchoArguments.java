cea4eb80f9017fa79d0e75236f66f00a0a19781c
spudmux <info@spudmux.com>
2014-10-21 18:12:11 +0100

    Sample project to test wildcard expansion with Ant

---
+++ src/EchoArguments.java
@@ -0,0 +1,20 @@
+
+
+
+/**
+ * Simple class which echos and arguments passed in back to the console.
+ */
+public class EchoArguments {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("Echoing Arguments:");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+	}
+
+
+	
+}
